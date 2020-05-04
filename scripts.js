269ca8cbdef48dce0b6e3835d9e9509fb5710fab
richardokpalax <okpala.richard@gmail.com>
2020-05-04 12:25:06 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,116 @@
 (function(){
+
+	var sessToken = localStorage.getItem("sessionToken");
+	var firstName; 
+	var lastName;
+	var birthdayMonth;
+	var birthdayDay;
+	var birthdayYear;
+	var birthday;
+	var email;
+	var phoneNumber;
+	var username;
+	var password;
+	var repPassword;
 	
+	function getProfilePic(){
+		var imageFound = false;
+		$.post("scripts.php",{"sessionToken":sessToken,"task":"get profile pic"},function(data){
+				if(data.hasPic == "false") return;
+				imageFound = true;
+				document.getElementById("profile-image-border").style= "background-image:url('"+data.pic+"')"
+			},"json");
+			return imageFound;
+	}//function getProfilePic()
+	
+	function loadAboutMe(){
+		document.getElementById("about-me-div").innerHTML = document.getElementById("about-me-textarea").value;
+	}
+	
+	var pageTask = document.getElementById("page").getAttribute("task");
+	
+	if(pageTask == "register"){
+	
+	var formDivs = document.getElementsByClassName("form-div");
+	
+	document.getElementById("back1").onclick = function(){
+		formDivs[0].classList.remove("hide");
+		formDivs[1].classList.add("hide");
+	}//back1
+	
+	document.getElementById("back2").onclick = function(){
+		formDivs[1].classList.remove("hide");
+		formDivs[2].classList.add("hide");
+	}//back1
+	
+	document.getElementById("back3").onclick = function(){
+		formDivs[2].classList.remove("hide");
+		formDivs[3].classList.add("hide");
+	}//back1
+	
+	document.getElementById("next1").onclick = function(){
+		firstName = document.getElementById("first-name").value;
+		lastName = document.getElementById("last-name").value;
+		// birthdayMonth = document.getElementById("birthday-month").value;
+		// birthdayDay = document.getElementById("birthday-day").value;
+		// birthdayYear = document.getElementById("birthday-year").value;
+		birthday = document.getElementById("birthday").value;
+		if(!firstName || !lastName || !birthday) return;
+		// birthday = birthdayYear+"-"+birthdayMonth+"-"+birthdayDay;//format birthday
+		formDivs[0].classList.add("hide");
+		formDivs[1].classList.remove("hide");
+	}//function
+	
+	//make it so when typing birthday the cursor jumps to next input
+	// document.getElementById("birthday-day").addEventListener("keyup", function(){
+	// 	var number = this.value.toString();
+	// 	if(number.length == 2){
+	// 		document.getElementById("birthday-month").focus();
+	// 		document.getElementById("birthday-month").select();
+	// 	}//if
+	// });
+	// document.getElementById("birthday-month").addEventListener("keyup", function(){
+	// 	var number = this.value.toString();
+	// 	if(number.length == 2){
+	// 		document.getElementById("birthday-year").focus();
+	// 		document.getElementById("birthday-year").select();
+	// 	}//if
+	// });
+	// //make sure the year isn't more than 4 digits
+	// document.getElementById("birthday-year").addEventListener("keydown",function(e){
+	// 	var value = this.value;
+	// 	var valueString = value.toString();
+	// 	if(valueString.length >= 4 && e.keyCode!=8){
+	// 		e.preventDefault();
+	// 	}//if
+	// });
+
+	document.getElementById("next2").onclick = function(){
+		if(!gender) return;
+		formDivs[1].classList.add("hide");
+		formDivs[2].classList.remove("hide");
+		
+	}//function
+		document.getElementById("male-img").onclick = function(){
+			this.classList.add("border1");
+			gender="male";
+			document.getElementById("female-img").classList.remove("border1");
+		}
+		document.getElementById("female-img").onclick = function(){
+			this.classList.add("border1");
+			gender="female";
+			document.getElementById("male-img").classList.remove("border1");
+		}
+	document.getElementById("next3").onclick = function(){
+		email = document.getElementById("email").value;
+		phoneNumber = document.getElementById("phone-number").value;
+		if(!email || !phoneNumber) return;
+		formDivs[2].classList.add("hide");
+		formDivs[3].classList.remove("hide");
+	}//function
+	}//if(pageTaks == "register")
+	
+	if(pageTask == "register" || pageTask == "login"){
 	var form = document.getElementById("form");
 	localStorage.removeItem('username',username);
 	
@@ -9,22 +120,24 @@
 	
 	//registration
 	if(task == "register"){
-	var firstName = document.getElementById("first-name").value;
-	var lastName = document.getElementById("last-name").value;
-	var username = document.getElementById("username").value;
-	var email = document.getElementById("email").value;
-	var phoneNumber = document.getElementById("phone-number").value;
-	var password = document.getElementById("password").value;
-	var repPassword = document.getElementById("rep-password").value;
 	
-	if(!firstName ||!lastName || !username || !email || !phoneNumber || !password || !repPassword) return;
+	username = document.getElementById("username").value;
+	password = document.getElementById("password").value;
+	repPassword = document.getElementById("rep-password").value;
+	
+	if(!firstName ||!lastName || !username || !email || !phoneNumber || !password || !repPassword || !gender || !birthday) return;
 	if(password.length < 6){
 		alert("Password must be at least 6 characters");
 		return;
 	};
-	if(password != repPassword) alert("Passwords don't match");
+	if(password != repPassword){ 
+		alert("Passwords don't match");
+		return;
+	}
 	
-	var postData = {"firstName":firstName, "lastName":lastName, "username":username, "phoneNumber":phoneNumber, "email":email, "password":password, "task":task};
+	var postData = {"firstName":firstName, "lastName":lastName, 
+	"username":username, "phoneNumber":phoneNumber, "email":email, 
+	"password":password, "task":task, "gender":gender, "birthday":birthday};
 	
 	$.post("scripts.php", postData, function(data){
 	
@@ -36,12 +149,15 @@
 			alert("That email is taken");
 			return;
 		}//if
-		window.location.replace("login.html");
+		window.location.replace("index.html");
+		localStorage.setItem("sessionToken",data);
 	});//post
 	}//task == "register"
 	
 	//login
+	if(pageTask == "login") localStorage.removeItem("sessionToken");
 	if(task == "login"){
+		
 		var username = document.getElementById("username").value;
 		var password = document.getElementById("password").value;
 		var code = document.getElementById("code").value;
@@ -62,18 +178,177 @@
 				setTimeout(function(){
    					alert("Please enter code that was texted");
 				}, 5);
+				return;
 			}//if
 			if(data == "wrong code"){
 				alert("Incorrect code");
-			}//if
-			if(data == "success"){
-				localStorage.setItem('username',username);
-				window.location.replace("index.html");
+				return;
 			}//if
 			
+				localStorage.setItem('username',username);
+				window.location.replace("index.html");
+				localStorage.setItem("sessionToken",data);
 		})//post
 	}//task == "login"
 	
-	}
+	}//on form submit
+	}//if(pageTask == "register" || pageTask == "login")
+	
+	//User is on profile page
+	if(document.getElementById("profile-pic-form")){
+		
+		getProfilePic();
+		
+		document.getElementById("profile-pic-hidden").value = sessToken;
+		//When changing profile image, submit form once image is selected
+		document.getElementById("profile-pic-input").onchange = function(){
+			document.getElementById("profile-pic-form").submit();
+			document.getElementById("profile-image-border").innerHTML = "<img class='loading-image' src='images/gears.gif'>"
+			getProfilePic();
+			var getPicInterval = setInterval(function(){
+				if(!getProfilePic()){
+					clearInterval(getPicInterval);
+					document.getElementById("profile-image-border").innerHTML = "";
+				}
+			},500);
+			
+		}
+		
+		//handle edit about-me toggle
+		document.getElementById("edit-toggle").onclick = function(){
+			document.getElementById("about-me-textarea").classList.remove("hide");
+			document.getElementById("about-me-div").classList.add("hide");
+		}
+		
+		//submit user info
+		document.getElementById("user-info-form").onsubmit = function(e){
+			e.preventDefault();
+			var formValues = $("#user-info-form").serialize();
+			$.post("scripts.php",{"sessToken":sessToken,"formValues":formValues},function(data){
+				document.getElementById("submit-feedback").innerHTML = "";
+				alert("Update!");
+			});
+			
+			//handle edit about-me toggle after submit
+			loadAboutMe()
+			document.getElementById("about-me-textarea").classList.add("hide");
+			document.getElementById("about-me-div").classList.remove("hide");
+			
+		}//onsubmit
+		
+		//get user info
+		$.post("scripts.php",{"sessToken":sessToken,"task":"loadinfo"},function(data){
+			
+			for(var i=0; i < data.returnArray.length; i++){
+				var formElements = document.getElementsByName(data.returnArray[i][0]);
+				for(var y=0; y < formElements.length; y++){
+					if(formElements[y].type=="checkbox" || formElements[y].type=="radio"){
+						if(formElements[y].value == data.returnArray[i][1]){
+							formElements[y].checked = true;
+						}//if
+					}//if
+					else{
+						formElements[y].value = data.returnArray[i][1]
+					}//else
+				}//for
+			}//for
+			
+			loadAboutMe()
+		},"json");
+		
+	}//if document.getElementById("profile-pic-form"
+	
+	
+	//Page that loads users when searching for partners
+	if(pageTask == "user-search"){
+	
+		$.post("scripts.php",{"sessToken":sessToken,"task":"loadusers"},function(data){
+			
+			document.getElementById("user-list-div").innerHTML = data.list;
+			
+		},"json");
+	
+	}//user-list-div
+	
+	//Chat.html
+	if(pageTask == "chat"){
+		alert()
+	}//if
 	
 })();
+
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    /*==================================================================
+    [ Show pass ]*/
+    var showPass = 0;
+    $('.btn-show-pass').on('click', function(){
+        if(showPass == 0) {
+            $(this).next('input').attr('type','text');
+            $(this).find('i').removeClass('fa-eye');
+            $(this).find('i').addClass('fa-eye-slash');
+            showPass = 1;
+        }
+        else {
+            $(this).next('input').attr('type','password');
+            $(this).find('i').removeClass('fa-eye-slash');
+            $(this).find('i').addClass('fa-eye');
+            showPass = 0;
+        }
+        
+    });
+    
+
+})(jQuery);
--- scripts.php
+++ scripts.php
@@ -2,15 +2,19 @@
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $mysqli = new mysqli("localhost","root","");
 $mysqli->select_db("fitlink");
-
 $task = $_POST["task"];
+
+if($task == "register" || $task == "login"){
 $password = $_POST["password"];
 $username = $_POST["username"];
+}
 
 if($task == "register"){
 
 $firstName = $_POST["firstName"];
 $lastName = $_POST["lastName"];
+$birthday = $_POST["birthday"];
+$gender = $_POST["gender"];
 $email = $_POST["email"];
 $phoneNumber = $_POST["phoneNumber"];
 $query = $mysqli->query("SELECT * FROM users WHERE email='$email'");
@@ -30,8 +34,9 @@ $salt = rand();
 $code = rand(100000,999999);
 $hashPassword = hash("sha256", $salt.$password, FALSE);
 $mysqli->query("INSERT INTO users VALUES ('','$firstName','$lastName','$username','$email','$phoneNumber','$hashPassword','$salt','$time','$code','0')");
+$mysqli->query("INSERT INTO users_info VALUES ('','$username','$birthday','$gender','','')");
 
-echo "success";
+echo $code;
 
 }//if $task = "register"
 
@@ -62,8 +67,123 @@ $hashPassword = hash("sha256", $salt.$password, FALSE);
 
 $code = rand(100000,999999);
 $mysqli->query("UPDATE users SET code1='$code' WHERE username='$username'");
-echo "success";
+echo $code;
 
 }//if $task = "login"
+
+elseif(isset($_FILES["profile-pic"])){
+	$code = $_POST["code"];
+	$query = $mysqli->query("SELECT * FROM users WHERE code1='$code'");
+	$get = $query->fetch_assoc();
+	$username = $get["username"];
+	$imageName = $_FILES["profile-pic"]["name"];
+	$imagePath = "images/uploads/".$imageName;
+	$check = getimagesize($_FILES["profile-pic"]["tmp_name"]);
+	if($check===false) return;
+	move_uploaded_file($_FILES["profile-pic"]["tmp_name"],$imagePath);
+	$mysqli->query("UPDATE users_info SET profile_pic='$imageName' WHERE username='$username'");
+	
+}//if $_FILES["profile-pic"]
+
+elseif($task == "get profile pic"){
+	$sessionToken = $_POST["sessionToken"];
+	$query = $mysqli->query("SELECT * FROM users WHERE code1='$sessionToken'");
+	$get = $query->fetch_assoc();
+	$username = $get["username"];
+	$query = $mysqli->query("SELECT * FROM users_info WHERE username='$username'");
+	$get = $query->fetch_assoc();
+	$profilePic = $get["profile_pic"];
+	$picturePath = "images/uploads/".$profilePic;
+	$hasPic = ($profilePic)? "true":"false";
+	$return["hasPic"] = $hasPic;
+	$return["pic"] = $picturePath;
+	echo json_encode($return);
+}//$task == "get profile pic"
+
+elseif(isset($_POST["formValues"])){
+	$sessionToken = $_POST["sessToken"];
+	$formValues = str_replace("+"," ",$_POST["formValues"]);//remove + that serialize adds
+	$formValues = $mysqli->real_escape_string($formValues);
+	$query = $mysqli->query("SELECT * FROM users WHERE code1='$sessionToken'");
+	$get = $query->fetch_assoc();
+	$username = $get["username"];
+	$query = $mysqli->query("UPDATE users_info SET info='$formValues' WHERE username='$username'");
+}//elseif 
+
+elseif($task == "loadinfo"){
+
+	$sessionToken = $_POST["sessToken"];
+	$query = $mysqli->query("SELECT * FROM users WHERE code1='$sessionToken'");
+	$get = $query->fetch_assoc();
+	$username = $get["username"];
+	$query = $mysqli->query("SELECT * FROM users_info WHERE username='$username'");
+	$get = $query->fetch_assoc();
+	
+	/*
+	$userInfo = str_replace("=",",",$get["info"]);
+	$infoArray = explode("&",$userInfo);
+		*/
+	$returnArray = array();
+	$x = 0;	
+	$infoArray = explode("&",$get["info"]);	
+	foreach($infoArray as $item){
+		$returnArray[$x] = explode("=",$item);
+		$x++;
+	}
+		
+	$return["returnArray"] = $returnArray;
+	
+	echo json_encode($return);
+}//elseif
+
+elseif($task == "loadusers"){
+	
+	$sessionToken = $_POST["sessToken"];
+	$query = $mysqli->query("SELECT * FROM users");
+	
+	$return["list"] = "";
+	
+	while($get = $query->fetch_array()){
+		
+		$username = $get["username"];
+		$fName = $get["first_name"];
+		$lName = $get["last_name"];
+		
+		$query2 = $mysqli->query("SELECT * FROM users_info WHERE username='".$username."'");
+		$get2 = $query2->fetch_assoc();
+		$profile_pic = $get2["profile_pic"];
+		$userInfo = $get2["info"];
+		
+		//get 'about me' from user info
+		if($userInfo){
+			$aboutMe = explode("=",$userInfo);
+			if($aboutMe[0] == "about-me"){
+				$aboutMe = $aboutMe[1];
+				$aboutMe = explode("&",$aboutMe);
+				$aboutMe = $aboutMe[0];
+			}//if
+			else $aboutMe = "";
+		}//if
+		else $aboutMe = "";
+		
+		if(!$profile_pic) $profile_pic = "profile-placeholder.png";
+		else $profile_pic = "uploads/".$profile_pic;
+		
+		$return["list"] .= "
+		<div class='user-div text-small' style='text-align:left'>
+		
+		<div class='profile-pic-list inline-top' style='background-image:url(\"images/".$profile_pic."\")'></div>
+		
+		<div class='inline-top' style='position:relative;width:100%;max-width:290px;height:100%;'>
+		".$fName." ".$lName."
+		<p>
+		".$aboutMe."
+		<div style='text-align:right;position:absolute;bottom:5px;right:0px'>Chat</div>
+		</div> 
+		</div>";
+	}//while
+	
+	echo json_encode($return);
+}
 
 ?>
